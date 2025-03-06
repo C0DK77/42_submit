ca0685c6102de639ffb9fe2d92fdd43fd584e0da
Corentin Desjars De Keranroue <cdesjars@bess-f4r1s5.clusters.42paris.fr>
2025-03-06 12:06:04 +0100

    libft

---
+++ printf/ft_printf_utils.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdesjars <cdesjars@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 16:06:31 by cdesjars          #+#    #+#             */
+/*   Updated: 2025/02/12 20:05:41 by cdesjars         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_putstr(char *str)
+{
+	int	count;
+
+	count = 0;
+	if (!str)
+		return (count += ft_putstr("(null)"));
+	while (*str)
+		count += ft_putchar(*str++);
+	return (count);
+}
+
+int	ft_putnbr_base_long(unsigned long nbr, char *base, unsigned long lbase,
+		int b)
+{
+	int	count;
+
+	count = 0;
+	if (nbr == 0 && b == 0)
+		return (count += ft_putstr("(nil)"));
+	if (b == 0)
+		count += ft_putstr("0x");
+	if (nbr >= lbase)
+		count += ft_putnbr_base_long(nbr / lbase, base, lbase, 1);
+	return (count += ft_putchar(base[nbr % lbase]));
+}
+
+int	ft_putnbr_base_int(unsigned int nbr, char *base, int b)
+{
+	int	count;
+
+	count = 0;
+	if (b == 0)
+		count += ft_putstr("0x");
+	if (nbr >= 16)
+		count += ft_putnbr_base_int(nbr / 16, base, 1);
+	return (count += ft_putchar(base[nbr % 16]));
+}
+
+int	ft_putnbr(int nb)
+{
+	int		count;
+	long	nbr;
+
+	count = 0;
+	nbr = nb;
+	if (nbr < 0)
+	{
+		count += ft_putchar('-');
+		nbr = -nbr;
+	}
+	if (nbr >= 10)
+		count += ft_putnbr(nbr / 10);
+	return (count += ft_putchar((nbr % 10) + 48));
+}
