3b670e7824c20bfa9b49fd63c6bb5f8ed8ff9bb9
Corentin Desjars De Keranroue <cdesjars@bess-f4r1s5.clusters.42paris.fr>
2025-03-06 12:09:45 +0100

    get_next_line

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdesjars <cdesjars@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/25 12:41:30 by cdesjars          #+#    #+#             */
+/*   Updated: 2025/02/25 13:32:19 by cdesjars         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stdint.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 8
+# endif
+
+//  FONCTIONS UTILS
+
+char	*ft_strcpy(char *d, char *s);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+void	*ft_calloc(size_t cnt, size_t len);
+
+//  FONCTIONS
+
+char	*get_next_line(int fd);
+char	*ft_cline(char *s);
+char	*ft_cstack(char *s, char *l);
+
+#endif
