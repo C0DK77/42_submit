8442dd7bea2052b70c3cfc252de6239a39f13e4c
codk <cod7k7@gmail.com>
2025-03-26 00:03:09 +0100

    edit

--- 2nd_cercle/minitalk/client.c
+++ 2nd_cercle/minitalk/client.c
@@ -3,10 +3,89 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: corentindesjars <corentindesjars@studen    +#+  +:+       +#+        */
+/*   By: codk <codk@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 13:18:07 by corentindes       #+#    #+#             */
-/*   Updated: 2025/03/24 13:18:09 by corentindes      ###   ########.fr       */
+/*   Updated: 2025/03/25 23:25:38 by codk             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ft_putchar (char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+    while (*str)
+        ft_putchar(*str++);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n == -2147483648)
+	{
+		ft_putstr("-2147483648");
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	ft_putchar((n % 10) + 48);
+}
+
+// char	ft_convert_char_to_bit(char c)
+// {
+// 	int		i;
+// 	char	*s;
+
+// 	i = 0;
+// 	s = (char *)malloc(sizeof(char) * 9);
+// 	if (!s)
+// 		return (NULL);
+// 	while (i < 8)
+// 	{
+// 		s[i] = (c * 2) + 48;
+// 		c /= 2;
+// 		i--;
+// 	}
+// }
+
+void	ft_send_char(pid_t pid, char c)
+{
+	int	i;
+
+	for (i = 0; i < 8; i++)
+	{
+		if ((c >> i) & 1)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
+void	ft_send_string(pid_t pid, char *str)
+{
+	while (*str)
+		ft_send_char(pid, *str++);
+	ft_send_char(pid, '\0');
+}
+
+int main (int ac, char **av)
+{
+    pid_t server_pid;
+    server_pid = atoi(av[1]);
+    ft_send_string(server_pid, av[2]);
+    return 0;
+}
--- 2nd_cercle/minitalk/server.c
+++ 2nd_cercle/minitalk/server.c
@@ -6,7 +6,79 @@
 /*   By: codk <codk@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 16:52:58 by codk              #+#    #+#             */
-/*   Updated: 2025/03/25 16:53:12 by codk             ###   ########.fr       */
+/*   Updated: 2025/03/25 23:28:12 by codk             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ft_putchar (char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+    while (*str)
+        ft_putchar(*str++);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n == -2147483648)
+	{
+		ft_putstr("-2147483648");
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	ft_putchar((n % 10) + 48);
+}
+
+void signal_handler (int sig, siginfo_t *info, void *context)
+{
+    static int bit;
+    static char c;
+    bit = 0;
+    if (sig == SIGUSR2)
+		c |= (1 << bit);
+    bit++;
+    if (bit == 8)
+    {
+        ft_putchar(c);
+        if (c == '\0')
+        {
+            ft_putstr("Fin de message");
+            ft_putchar('\n');
+        }
+        c = 0;
+        bit = 0;
+    }
+}
+
+int main (void)
+{
+    struct sigaction    sa;
+    // memset(&sa, 0, sizeof(sa));
+    sa.sa_sigaction = signal_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    sigaction(SIGUSR1, &sa, NULL); // bit = 0
+	sigaction(SIGUSR2, &sa, NULL); // bit = 1
+    
+    ft_putstr("🔔 PID du processus : ");
+    ft_putnbr(getpid());
+    ft_putstr("\n📬 En attente d'un signal ...\n");
+    while (1)
+        pause();
+}
