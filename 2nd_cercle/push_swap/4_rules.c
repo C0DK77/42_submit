52b44c8e16c9048c82940aa87481adf760f9f91e
Corentin Desjars <corentindesjars@MacBook-Air-de-Corentin.local>
2025-04-02 22:56:57 +0200

    push swap

--- 2nd_cercle/push_swap/4_rules.c
+++ 2nd_cercle/push_swap/4_rules.c
@@ -6,17 +6,18 @@
 /*   By: corentindesjars <corentindesjars@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 11:35:56 by corentindes       #+#    #+#             */
-/*   Updated: 2025/04/02 22:21:58 by corentindes      ###   ########.fr       */
+/*   Updated: 2025/04/02 22:56:33 by corentindes      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "push_swap.h"
 
-void	ft_split_list_between_a_and_b(t_ps_list **pile_a, t_ps_list **pile_b, int ac)
+void	ft_split_list_between_a_and_b(t_ps_list **pile_a, t_ps_list **pile_b,
+		int ac)
 {
 	int	i;
-	int count_b;
+	int	count_b;
 
 	i = 1;
 	count_b = 0;
@@ -27,7 +28,7 @@ void	ft_split_list_between_a_and_b(t_ps_list **pile_a, t_ps_list **pile_b, int a
 		// 	ft_list_swap(pile_a);
 		// }
 		// if ((*pile_b)->target_node == (*pile_b)->next->target_node + 1
-			// && pile_b && *pile_b && (*pile_b)->next)
+		// && pile_b && *pile_b && (*pile_b)->next)
 		// {
 		// 	ft_list_swap(pile_b);
 		// }
@@ -53,11 +54,12 @@ void	ft_split_list_between_a_and_b(t_ps_list **pile_a, t_ps_list **pile_b, int a
 	}
 }
 
-// NE MARCHE PAS 
+// NE MARCHE PAS
 
-void	ft_push_in_a (t_ps_list **pile_a, t_ps_list **pile_b)
+void	ft_push_in_a(t_ps_list **pile_a, t_ps_list **pile_b)
 {
-	t_ps_list *temp;
+	t_ps_list	*temp;
+
 	temp = *pile_b;
 	*pile_b = (*pile_b)->next;
 	temp->next = *pile_a;
@@ -66,29 +68,44 @@ void	ft_push_in_a (t_ps_list **pile_a, t_ps_list **pile_b)
 
 //	FONCTIONS POUR AC EN DESSOUS DE 4
 
-void ft_rules_2_elements (t_ps_list **pile)
+void	ft_rules_2_elements(t_ps_list **pile)
 {
-	if (ft_verif_classement(pile)==0)
-		return;
+	if (ft_verif_classement(pile) == 0)
+		return ;
 	else
 		ft_list_swap(pile);
 }
 
 //	FONCTIONS POUR AC EN DESSOUS DE 5
 
-void ft_rules_3_elements (t_ps_list **pile)
+void	ft_rules_3_elements(t_ps_list **pile)
 {
-	t_ps_list *temp;
-	int max_value;
+	t_ps_list	*temp;
+	int			max_value;
+	int			min_value;
+
 	temp = *pile;
-	//int min_value;
-	if (ft_verif_classement(pile)==0)
-		return;
+	max_value = temp->nbr;
+	min_value = temp->nbr;
+	if (ft_verif_classement(pile) == 0)
+		return ;
 	while (temp->next)
 	{
 		if (temp->nbr < temp->next->nbr)
 			max_value = temp->next->nbr;
-			temp = temp->next;
+		if (temp->nbr > temp->next->nbr)
+			min_value = temp->next->nbr;
+		temp = temp->next;
 	}
 	printf("max_value : %i\n", max_value);
+	printf("min_value : %i\n", min_value);
+	temp = *pile;
+	while (temp->next)
+	{
+		if (max_value = temp->nbr && )
+			max_value = temp->nbr;
+		if (temp->nbr > temp->next->nbr)
+			min_value = temp->next->nbr;
+		temp = temp->next;
+	}
 }
