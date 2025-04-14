cff8a4aa2dada86024a7f14acf9736e3ed700367
codk <cod7k7@gmail.com>
2025-04-14 19:29:01 +0200

    rrr

--- 2nd_cercle/push_swap/4_rules.c
+++ 2nd_cercle/push_swap/4_rules.c
@@ -6,7 +6,7 @@
 /*   By: codk <codk@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 11:35:56 by corentindes       #+#    #+#             */
-/*   Updated: 2025/04/14 17:40:33 by codk             ###   ########.fr       */
+/*   Updated: 2025/04/14 19:14:23 by codk             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,32 +25,41 @@ void	ft_split_list_between_a_and_b(t_ps_list **pile_a, t_ps_list **pile_b,
 	count_b = 0;
 	while (i <= ac)
 	{
-		if ((*pile_a)->target_node <= (ac - 1) / 3)
+		if ((*pile_a) && (*pile_a)->next && (*pile_a)->target_node == (int)((*pile_a)->next->target_node) + 1)
+			ft_list_swap(pile_a);
+		if ((*pile_b) && (*pile_b)->next && (int)((*pile_b)->target_node) + 1 == (int)(*pile_b)->next->target_node)
+			ft_list_swap(pile_b);
+		
+		if ((*pile_a) && (*pile_a)->target_node <= (ac - 1) / 3)
 		{
 			ft_list_push(pile_a, pile_b);
 			ft_list_rotate(pile_b);
 			count_a--;
 			count_b++;
 		}
-		else if ((ac - 1) / 3 < (*pile_a)->target_node
+		else if ((*pile_a) && (ac - 1) / 3 < (*pile_a)->target_node
 		&& (*pile_a)->target_node <= ((ac - 1) / 3) * 2)
 		{
 			ft_list_push(pile_a, pile_b);
 			count_a--;
 			count_b++;
 		}
-		else if (((ac - 1) / 3) * 2 < (*pile_a)->target_node)
+		else if ((*pile_a) && ((ac - 1) / 3) * 2 < (*pile_a)->target_node)
 			ft_list_rotate(pile_a);
 		if (count_b == 2)
 			ft_rules_2_elements('b', pile_b);
-		if (ac - i == 2)
+		if (count_a == 2)
 			ft_rules_2_elements('a', pile_a);
-		if ((*pile_a)->target_node == (int)((*pile_b)->target_node) + 1 && !(*pile_b))
+		if ((*pile_b) && (*pile_a) && (*pile_a)->target_node == (int)((*pile_b)->target_node) + 1)
 		{
 			ft_list_push(pile_a, pile_b);
 			count_a--;
 			count_b++;
 		}
+		if ((*pile_a) && (*pile_a)->next && (*pile_a)->target_node == (int)((*pile_a)->next->target_node) + 1)
+			ft_list_swap(pile_a);
+		if ((*pile_b) && (*pile_b)->next && (int)((*pile_b)->target_node) + 1 == (int)(*pile_b)->next->target_node)
+			ft_list_swap(pile_b);
 		i++;
 	}
 }
