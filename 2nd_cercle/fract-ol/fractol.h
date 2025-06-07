73b0222e3fac22c940bf8a22b10c39d382d12105
Corentin Desjars <corentindesjars@MacBook-Air-de-Corentin.local>
2025-06-07 19:11:11 +0200

    hhh

--- 2nd_cercle/fract-ol/fractol.h
+++ 2nd_cercle/fract-ol/fractol.h
@@ -3,30 +3,71 @@
 
 # include "mlx.h"
 # include <stdlib.h>
+# include <math.h>
+# include <pthread.h>
 
-typedef struct s_vars
+# define WIDTH 700
+# define HEIGH 700
+# define THREAD_WIDTH 7
+# define THREAD_NUMBER 100
+# define ESC 53
+# define UP 126
+# define DOWN 125
+# define LEFT 123
+# define RIGHT 124
+# define R 15
+# define C 8
+# define H 4
+# define J 38
+# define P 35
+# define M 46
+# define SCROLL_UP 4
+# define SCROLL_DOWN 5
+
+typedef struct s_mlx
 {
 	void	*mlx;
-	void	*win;
-	void	*img;
+	void	*window;
+	void	*image;
 	char	*addr;
 	int		bpp;
 	int		line_len;
 	int		endian;
-	int		width;
-	int		height;
 	double	re_min;
 	double	re_max;
 	double	im_min;
 	double	im_max;
 	int		max_iter;
-}			t_vars;
+}			t_mlx;
 
-int			key_hook(int keycode, t_vars *vars);
-int			close_program(t_vars *vars);
-int			resize_hook(int x, int y, int width, int height, t_vars *vars);
-void		draw_fractal(t_vars *v);
-void		my_pixel_put(t_vars *v, int x, int y, int color);
-int			expose_hook(t_vars *vars);
+int			key_hook(int keycode, t_mlx *vars);
+int			close_program(t_mlx *vars);
+int			resize_hook(int x, int y, int width, int height, t_mlx *vars);
+void		draw_fractal(t_mlx *v);
+void		my_pixel_put(t_mlx *v, int x, int y, int color);
+int			expose_hook(t_mlx *vars);
 
 #endif
+
+typedef struct s_fractal
+{
+	void	*mlx;
+	void	*window;
+	void	*image;
+	void	*pointer_to_image;
+	int		bits_per_pixel;
+	int		size_line;
+	int		endian;
+	int		x;
+	int		y;
+	double	zx;
+	double	zy;
+	double	cx;
+	double	cy;
+	int		color;
+	double	offset_x;
+	double	offset_y;
+	double	zoom;
+	char	*name;
+	int		max_iterations;
+}			t_fractal;
--- 2nd_cercle/fract-ol/main.c
+++ 2nd_cercle/fract-ol/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.c                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codk <codk@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: corentindesjars <corentindesjars@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 23:21:02 by codk              #+#    #+#             */
-/*   Updated: 2025/05/22 21:17:08 by codk             ###   ########.fr       */
+/*   Updated: 2025/06/07 19:09:37 by corentindes      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,52 @@ int	main(void)
 	mlx_hook(vars.win, 17, 0L, close_program, &vars);
 	mlx_expose_hook(vars.win, expose_hook, &vars);
 	mlx_loop(vars.mlx);
+	return (0);
+}
+//
+
+int	draw_fractal(t_fractal *fractal, char *query)
+{
+	if (ft_strncmp(query, "mandel", 7) == 0)
+		draw_mandelbrot(fractal);
+	else if (ft_strncmp(query, "julia", 6) == 0)
+	{
+		if (!fractal->cx && !fractal->cy)
+		{
+			fractal->cx = -0.745429;
+			fractal->cy = 0.05;
+		}
+	draw_julia(fractal);
+}
+else if (ft_strncmp(query, "ship", 5) == 0)
+	draw_burning_ship(fractal);
+else
+{
+	ft_putendl_fd("Available fractals: mandel, julia, ship", 1);
+	exit_fractal(fractal);
+}
+mlx_put_image_to_window(fractal->mlx, fractal->window, fractal->image, 0,
+	0);
+return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_fractal	*fractal;
+
+	if (argc != 2)
+	{
+		ft_putendl_fd("Usage: ./fractol <fractal>", 1);
+		ft_putendl_fd("Available fractals: mandelbrot, julia, burningship", 1);
+		return (0);
+	}
+	fractal = malloc(sizeof(t_fractal));
+	init_fractal(fractal);
+	init_mlx(fractal);
+	mlx_key_hook(fractal->window, key_hook, fractal);
+	mlx_mouse_hook(fractal->window, mouse_hook, fractal);
+	mlx_hook(fractal->window, 17, 0L, exit_fractal, fractal);
+	draw_fractal(fractal, argv[1]);
+	mlx_loop(fractal->mlx);
 	return (0);
 }
