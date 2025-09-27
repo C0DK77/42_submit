0ec6fa63036d159fe66b2d941d3d09390d7db546
St-el-a-lisa <stelacid@gmail.com>
2025-09-27 17:28:43 +0200

    norm again

--- 3rd_cercle/minishell/srcs/shell/shell_loop.c
+++ 3rd_cercle/minishell/srcs/shell/shell_loop.c
@@ -6,36 +6,49 @@
 /*   By: ecid <ecid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/17 15:23:32 by ecid              #+#    #+#             */
-/*   Updated: 2025/09/27 16:30:24 by ecid             ###   ########.fr       */
+/*   Updated: 2025/09/27 17:26:44 by ecid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static char	*shell_readline(void)
+{
+	char	*line;
+
+	line = readline(ft_env_prompt());
+	if (!line)
+		printf("exit\n");
+	else if (*line)
+		add_history(line);
+	return (line);
+}
+
+static t_parsing	*parse_from_line(char *line, t_envp *env)
+{
+	t_token		*tokens;
+	t_parsing	*ast;
+
+	tokens = ft_token(line, env);
+	if (!tokens || !ft_token_check(tokens))
+	{
+		ft_token_free(tokens);
+		return (NULL);
+	}
+	ast = ft_parse_line(tokens);
+	ft_token_free(tokens);
+	return (ast);
+}
 
 int	ft_shell_loop(t_envp **c_envp)
 {
 	char		*line;
-	t_token		*tokens;
 	t_parsing	*parse;
 
-	line = readline(ft_env_prompt());
+	line = shell_readline();
 	if (!line)
-	{
-		printf("exit\n");
 		return (0);
-	}
-	if (*line)
-		add_history(line);
-	tokens = ft_token(line, *c_envp);
-	if (!tokens || !ft_token_check(tokens))
-	{
-		ft_token_free(tokens);
-		free(line);
-		return (1);
-	}
-	parse = ft_parse_line(tokens);
-	ft_token_free(tokens);
+	parse = parse_from_line(line, *c_envp);
 	free(line);
 	if (!parse)
 		return (1);
@@ -45,4 +58,3 @@ int	ft_shell_loop(t_envp **c_envp)
 	setup_signals();
 	return (1);
 }
-
